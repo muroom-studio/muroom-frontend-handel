bc1d1558f8f9f3d800bf3ed49b4ae8c1525a9637
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2025-11-30 17:20:03 +0900

    🐛 [fix] production에서 인터셉트 라우팅 작동안되는 버그 해결

--- apps/web/src/app/@modal/(.)welcome/page.tsx
+++ apps/web/src/app/@modal/(.)welcome/page.tsx
@@ -3,6 +3,8 @@
 import WelcomePage from '@/components/welcome';
 import { useResponsiveLayout } from '@/hooks/useResponsiveLayout';
 
+export const dynamic = 'force-dynamic';
+
 export default function InterceptedPage() {
   const { isMobile } = useResponsiveLayout();
 
