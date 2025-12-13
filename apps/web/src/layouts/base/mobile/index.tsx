4fee692eb80f08f27134ccc844bc83039ef417b3
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2025-12-14 00:28:42 +0900

    🐛 [fix] mobile 인디케이터 부분만큼 pb 적용
    OnSNB -> base 레이아웃으로

--- apps/web/src/layouts/base/mobile/index.tsx
+++ apps/web/src/layouts/base/mobile/index.tsx
@@ -3,5 +3,5 @@ interface Props {
 }
 
 export default function BaseMobileLayout({ children }: Props) {
-  return <>{children}</>;
+  return <div className='pb-[env(safe-area-inset-bottom)]'>{children}</div>;
 }
