af8b983db3d29425ae4a5bdc3e8e0570d44b8e42
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2026-02-18 22:26:23 +0900

    🔥 [remove] ga4 에러 수집용 더미 코드 제거

--- apps/musician/src/app/(onSNB)/home/page.tsx
+++ apps/musician/src/app/(onSNB)/home/page.tsx
@@ -6,9 +6,5 @@ import { useResponsiveLayout } from '@/hooks/common/useResponsiveLayout';
 export default function Page() {
   const { isMobile } = useResponsiveLayout();
 
-  if (typeof window !== 'undefined') {
-    throw new Error('GA4 증적을 위한 의도적 렌더링 에러!');
-  }
-
   return <HomePage isMobile={isMobile} />;
 }
