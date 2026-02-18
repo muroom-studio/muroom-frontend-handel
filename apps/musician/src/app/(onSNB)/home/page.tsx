71bfe4096f3b0aedd5db461c9da9c1d6cb5a74b4
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2026-02-18 22:14:59 +0900

    🧪 [test] ga4 exception이벤트 수집되는지 확인 (삭제예정)

--- apps/musician/src/app/(onSNB)/home/page.tsx
+++ apps/musician/src/app/(onSNB)/home/page.tsx
@@ -1,18 +1,14 @@
 'use client';
 
-import { useEffect } from 'react';
-
 import HomePage from '@/components/home';
 import { useResponsiveLayout } from '@/hooks/common/useResponsiveLayout';
-import { customFetch } from '@/utils/customFetch';
 
 export default function Page() {
   const { isMobile } = useResponsiveLayout();
 
-  useEffect(() => {
-    // 존재하지 않는 경로로 요청을 보내 404를 유도합니다.
-    customFetch('/trigger-error-for-ga');
-  }, []);
+  if (typeof window !== 'undefined') {
+    throw new Error('GA4 증적을 위한 의도적 렌더링 에러!');
+  }
 
   return <HomePage isMobile={isMobile} />;
 }
