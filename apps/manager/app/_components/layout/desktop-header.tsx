95efab10ce6c67d814bcc725c7930f616fa4ea95
Eunzo <wannabeeung@gmail.com>
2026-03-01 09:29:31 +0900

    💄 데스크탑 헤더 퍼블리싱 완료

--- apps/manager/app/_components/layout/desktop-header.tsx
+++ apps/manager/app/_components/layout/desktop-header.tsx
@@ -6,8 +6,17 @@ import BaseLogo from '@muroom/ui/assets/base-logo.svg';
 export default function DesktopHeader() {
   return (
     <header className='border-b-[0.5px] border-b-gray-300 p-5'>
-      <Link href='/'>
-        <Image src={BaseLogo} alt='로고' width={133} height={32} priority />
+      <Link href='/' className='flex items-center gap-2'>
+        <Image
+          src={BaseLogo}
+          alt='로고'
+          width={133}
+          height={32}
+          priority
+          className='brightness-0'
+        />
+
+        <span className='text-[22px] font-semibold'>파트너스</span>
       </Link>
     </header>
   );
